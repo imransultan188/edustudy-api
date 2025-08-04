a0e95693e3970ef2210a2f94ec9e4f641a67c6b0
imransultan188 <imransultan188@gmail.com>
2025-08-04 14:57:32 +0500

    fixing changes in env

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::get('/universities', [UniversityController::class, 'index']);
 Route::get('/levels', [LevelController::class, 'index']);
 
 Route::get('/test', fn() => response()->json(['msg' => 'ok']));
+Route::get('/testing', fn() => response()->json(['msg' => 'ok']));
 
 
 Route::middleware('auth:api')->group(function () {
